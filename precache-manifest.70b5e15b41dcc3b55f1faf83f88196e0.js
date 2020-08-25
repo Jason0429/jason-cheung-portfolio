681dba07f04090ce82667c460f2ef12cd112f485
Jason <jasoncheung0429@gmail.com>
2020-08-25 14:40:30 -0400

    Updates

---
+++ precache-manifest.70b5e15b41dcc3b55f1faf83f88196e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd5c2393e5f4cc9b3ddfa6819516d7a",
+    "url": "/jason-cheung-portfolio/index.html"
+  },
+  {
+    "revision": "6018707f0fd761f10f14",
+    "url": "/jason-cheung-portfolio/static/css/main.2744df97.chunk.css"
+  },
+  {
+    "revision": "bb81fdcf3f9b9289b9f4",
+    "url": "/jason-cheung-portfolio/static/js/2.793b5ae0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jason-cheung-portfolio/static/js/2.793b5ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6018707f0fd761f10f14",
+    "url": "/jason-cheung-portfolio/static/js/main.e520840e.chunk.js"
+  },
+  {
+    "revision": "f874978eb6829eb13045",
+    "url": "/jason-cheung-portfolio/static/js/runtime-main.43ce25e0.js"
+  },
+  {
+    "revision": "a378fb07b39b941217820d0063b38b77",
+    "url": "/jason-cheung-portfolio/static/media/profile-pic-2.a378fb07.png"
+  }
+]);
